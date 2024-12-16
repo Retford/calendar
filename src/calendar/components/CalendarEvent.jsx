b972596b74ea892cad641827b464b1d244eefce8
Retford <retfordjb@gmail.com>
2024-12-16 13:56:34 -0500

    '-' between title and user name added

--- src/calendar/components/CalendarEvent.jsx
+++ src/calendar/components/CalendarEvent.jsx
@@ -4,7 +4,7 @@ export const CalendarEvent = ({ event }) => {
   return (
     <>
       <strong>{title}</strong>
-      <span>{user.name}</span>
+      <span> - {user.name}</span>
     </>
   );
 };
